cc0b1daf79196c2161b7e9230ca1c193351adaeb
조의정 <@DESKTOP-OVMR1CT>
2019-01-11 12:20:24 +0900

    수업예제 업로드

---
+++ src/web0110/bin.java
@@ -0,0 +1,16 @@
+package web0110;
+
+public class bin {
+		private String name;
+		private String email;
+		public bin(String name, String email) {
+			super();
+			this.name = name;
+			this.email = email;
+		}
+		
+		
+	
+	
+	
+}
